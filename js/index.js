7788ef3c12e5aa4b49785d612537707f202e1686
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2023-07-21 20:38:49 -0300

    testando

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ const openMenu = document.querySelector('#openMenu');
 const closeMenu = document.querySelector('#closeMenu');
 
 const menu = document.querySelector('.container-menu');
+const content = document.querySelector('.container-landing-page')
 
 
 openMenu.addEventListener("click", () => {
     menu.style.display = "block";
+    content.style.display = "none"
 })
 
 closeMenu.addEventListener("click", () => {
     menu.style.display = "none";
+    content.style.display = "block";
 })
